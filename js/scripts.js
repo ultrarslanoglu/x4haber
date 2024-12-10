3882278d8f9b242d0cfa17699ae0c29fde15bfe7
ultrarslanoglu <github@ultrarslanoglu.com>
2024-12-10 06:42:16 +0300

    Projeyi düzenledik ve yeni klasör yapısını oluşturduk

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Sayfa yüklendiğinde çalışacak JavaScript kodları buraya eklenecek
+    console.log('Sayfa yüklendi ve JavaScript çalışıyor!');
+});
